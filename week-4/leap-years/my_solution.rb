dec35802c5ebd08f43af5e2bc149d340bdc9f696
Ronesh Ghoshal <roneshghoshal@Ronesh-Ghoshals-MacBook-Pro.local>
2016-01-21 19:42:51 -0800

    Upload flow control exercises 4.5

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,15 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def leap_year?(year)
+  if year%4 != 0
+    false
+  elsif (year %100 == 0) &&  (year %400 != 0)
+    false
+  else true
+  end
+end
---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,10 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+
+  (a+b > c) && (b+c > a) && (a+c > b)
+
+end

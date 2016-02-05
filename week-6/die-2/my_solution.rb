95b3e75956539913e0aeb45b65f07ed97d915afd
Ronesh Ghoshal <roneshghoshal@Ronesh-Ghoshals-MacBook-Pro.local>
2016-02-04 18:37:55 -0800

    Add reflection for 6.2

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -69,3 +69,17 @@ class Die
 end
 
 # Reflection
+
+# The main difference between this class and the previous die class was that this time, our input
+# was an array of strings rather than an integer. The logic was mostly the same - we just had to calculate
+# the length of that array in our initialize method, and then in the roll method, we had to calculate a random
+# number that could represent the array INDEX. So, in a six-element array, we would calculate a random
+# number between 0..5 rather than 1..6.
+
+# This exercise was a good example of how it can be very convenient to write easily modifiable code. We barely
+# had to change anything from the previous die class.
+
+# I didn't learn any new methods on this challenge, but got some good practice using rand.
+
+# As far as concepts related to classes, I also got more comfortable using instance variables and initializing classes.
+
